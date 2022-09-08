8c2d16064306ab3f1a6d06cca338bb7a9ec7bd8e
sanddlekfx <sanddlexown@hotmail.com>
2022-09-08 14:59:01 -0300

    feat: adicionados arquivos estáticos

---
+++ resources/views/product.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="stylesheet" href="/css/style.css">
+        <script src="/js/scripts.js"></script>
+
+    </head>
+    <body>
+        <h1>Tela de Produtos</h1>
+        
+    </body>
+</html>
